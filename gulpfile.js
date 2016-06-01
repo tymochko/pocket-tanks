2ff099aef1549f15eed42acd1c985a7a614f5a92
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-06-01 11:05:16 +0300

    LVNODQIR-3 Create compilling sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
 
 gulp.task('es6', () => {
     return gulp.src('src/app.js')
@@ -9,6 +10,13 @@ gulp.task('es6', () => {
         .pipe(gulp.dest('build'));
 });
 
+gulp.task( 'sass', () => {
+	gulp.src( 'src/scss/main.scss' )
+		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( gulp.dest( 'src/public/stylesheets/' ) );
+});
+
 gulp.task('default', ['es6'], () => {
-    gulp.watch('src/app.js', ['es6'])
+    gulp.watch('src/app.js', ['es6']);
+    gulp.watch( 'src/scss/**/*.scss', ['sass'] );
 });
