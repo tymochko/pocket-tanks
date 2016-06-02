9d17caae942f7e82bf4dfaa64e15479cc7c6ec52
Yuri Tymochko <rain.yura@gmail.com>
2016-06-02 13:42:15 +0300

    Merged current progress from branch dbusers as a bugfix

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// require mongoose dep - MongoDB Object modeling
+const mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// connect to user database
+mongoose.connect('localhost:27017/users');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- src/server/models/users.js
+++ src/server/models/users.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-    userName: {type: String, required: true},
+    userName: {type: String, required: true, unique: true},
     userEmail: {type: String, required: true, unique: true},
     userPassword: {type: String, required: true},
-    userOnline: {type: Boolean}
+    isOnline: {type: Boolean}
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('usersCollection', userSchema);
