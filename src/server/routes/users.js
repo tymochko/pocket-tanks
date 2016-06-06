988699641caa740a9d0328b349fa3b066a3b1c7a
Yuri Tymochko <rain.yura@gmail.com>
2016-06-06 23:14:34 +0300

    LVNODQIR-28 created test update request

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -5,11 +5,12 @@ var router = express.Router();
 const usersCollection = require('../models/users');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-    usersCollection.find((err, users) => {
-        res.send(users);
-    });
-});
+// router.get('/', (req, res) => {
+//     usersCollection.find((err, users) => {
+//         console.log(users);
+//         res.send(users);
+//     });
+// });
 
 /* TODO
 
@@ -19,5 +20,36 @@ router.get('/', (req, res) => {
 4) user profile page change info request
 
 */
+
+router.get('/', (req, res) => {
+    usersCollection.find((err, users) => {
+        console.log(users);
+        res.send(users);
+    });
+});
+
+router.put('/update/:id', (req, res) => {
+    var id = req.params.id;
+    usersCollection.findOne({_id: id}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send();
+        } else {
+            if (!foundUser) {
+                res.status(404).send();
+            } else {
+                if (req.body.name) {
+                    foundUser.name = req.body.name;
+                }
+
+                if (req.body.password) {
+                    foundUser.password = req.body.password;
+                }
+            }
+        }
+
+        console.log(foundUser);
+    });
+});
 
 module.exports = router;
