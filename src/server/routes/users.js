5dc399ba4ef0c3c9bfbaeb56534123039d857e49
Yuri Tymochko <rain.yura@gmail.com>
2016-06-08 12:50:26 +0300

    LVNODQIR-28 added update user and add user routes

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -18,7 +18,9 @@ const usersCollection = require('../models/users');
 1) registration/login check request
 2) dashboard online users request
 3) user profile page info request
-4) user profile page change info request
+4) user profile page change userName request
+5) user profile page change userPassword request
+6) password requirements
 
 */
 
@@ -54,13 +56,11 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/add', (req, res) => {
-    var newUser = new User();
+    var newUser = new usersCollection();
 
     newUser.userName = req.body.userName;
     newUser.userEmail = req.body.userEmail;
     newUser.userPassword = req.body.userPassword;
-
-    console.log(newUser);
 
     newUser.save((err, savedObject) => {
         if (err) {
@@ -73,30 +73,47 @@ router.post('/add', (req, res) => {
     });
 });
 
-// curl -H "Content-Type: application/json" -X POST -d '{"userName":"Tom", "userEmail":"tom@example.com", "userPassword":"qweqwe"}' http://localhost:3000/users/add
+// router.put('/update/:id', (req, res) => {
+//     usersCollection.findOneAndUpdate({
+//         _id: req.params.id
+//     },{
+//         $set: {
+//             userName: req.body.userName,
+//             userPassword: req.body.userPassword
+//         }
+//     }, {upset: true}, (err, updatedUser) => {
+//         if (err) {
+//             console.log('error occured');
+//         } else {
+//             console.log(updatedUser);
+//             res.status(204);
+//         }
+//     });
+// });
 
-router.put('/update', (req, res) => {
-    var id = req.body;
-    console.log(id);
-    usersCollection.findOne({_id: id}, (err, foundUser) => {
+router.put('/update/:id', (req, res) => {
+    usersCollection.findOneAndUpdate({
+        _id: req.params.id
+    },{
+        $set: {
+            userName: req.body.userName,
+            userPassword: req.body.userPassword
+        }
+    }, {upset: true}, (err, updatedUser) => {
         if (err) {
-            console.log(err);
+            console.log('error occured ' + err);
             res.status(500).send();
         } else {
-            if (!foundUser) {
-                res.status(404).send();
-            } else {
-                if (req.body.name) {
-                    foundUser.name = req.body.name;
-                }
-
-                if (req.body.password) {
-                    foundUser.password = req.body.password;
-                }
+            if (req.body.name) {
+                updatedUser.name = req.body.name;
             }
-        }
 
-        res.send(foundUser);
+            if (req.body.password) {
+                updatedUser.password = req.body.password;
+            }
+
+            res.status(204);
+        }
     });
 });
 
