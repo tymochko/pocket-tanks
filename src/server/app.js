cb47e9551f8669ed02b8f8f53d6480203b50570d
Maksym <maksym08@gmail.com>
2016-06-08 15:27:25 +0300

    LVNODQIR-22 Started to work on chat

--- src/server/app.js
+++ src/server/app.js
@@ -32,6 +32,8 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/game', game);
 
+// started to work on chat
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
