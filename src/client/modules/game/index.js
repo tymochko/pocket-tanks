ada0af2f0d79b4e79a6cec31304c0c42a650ba0d
vvictoria <viniarska.v@gmail.com>
2016-06-24 15:20:19 +0300

    refactored file names and deleted old files

--- src/client/modules/game/index.js
+++ src/client/modules/game/index.js
@@ -8,12 +8,6 @@ module.exports = angular.module('tanks.game', [
 RouteConfig.$inject = ['$routeProvider'];
 function RouteConfig($routeProvider) {
     $routeProvider.when('/game', {
-        controller: GameCtrl,
-        templateUrl: 'controllers/game/game.html'
+        templateUrl: 'game/game.html'
     });
-};
-
-function GameCtrl($scope, $location) {
-    $location.url('http://localhost:3000/src/client/views/home.html');
-    console.log("required dashboard!");
 };
