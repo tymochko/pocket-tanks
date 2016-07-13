0d4c17dde239642cd8fd0c1c9b677ab33ebcc5ff
Yuri Tymochko <rain.yura@gmail.com>
2016-07-13 17:00:14 +0300

    LVNODQIR-81 implemented 2 tests for getProfile route

--- test/client/modules/profile/index.spec.js
+++ test/client/modules/profile/index.spec.js
@@ -1,5 +1,12 @@
+const testJson = {
+    userName: "ingrid",
+    userAge: 30,
+    userEmail: "ingrid@example.com"
+};
+
 describe("Test profile page", function () {
     var profileService, httpBackend;
+    const profileURL = 'api/users/profile';
 
     beforeEach(angular.mock.module("tanks.profile"));
 
@@ -8,11 +15,55 @@ describe("Test profile page", function () {
         httpBackend = _$httpBackend_;
     }));
 
-    fit("check for true or false", function () {
-        const profileURL = 'api/users/profile';
+    // tests for 'getProfile' route
+    fit("'getProfile' route should return object with 3 properties", function () {
 
-        httpBackend.whenRoute('PUT', profileURL + '/updateUser/');
-        expect(profileService.update).toBeDefined();
+        httpBackend.when('GET', profileURL)
+            .respond(testJson);
+        expect(testJson).toEqual({
+            userName: "ingrid",
+            userAge: 30,
+            userEmail: "ingrid@example.com"
+        });
     });
+
+    fit("'getProfile' route to return object, not an array", function () {
+
+        httpBackend.when('GET', profileURL)
+            .respond(testJson);
+        expect(testJson).not.toEqual([1, 3]);
+    });
+
+    fit("'getProfile' route to return object, not boolean", function () {
+
+        httpBackend.when('GET', profileURL)
+            .respond(testJson);
+        expect(testJson).not.toEqual(true);
+    });
+
+    // fit("'getProfile' route should not return userAge property not an integer", function () {
+    //
+    //     httpBackend.when('GET', profileURL)
+    //         .respond(testJson);
+    //     expect(testJson.userAge).not.toEqual('Hello');
+    // });
+    //
+    // fit("'getProfile' route should not return userAge property not an integer", function () {
+    //
+    //     httpBackend.when('GET', profileURL)
+    //         .respond(testJson);
+    //     expect(testJson.userAge).not.toEqual(true);
+    // });
+
+    // fit("'getProfile' route should not return userAge property not an integer", function () {
+    //
+    //     httpBackend.when('GET', profileURL)
+    //         .respond({
+    //             userAge: 'Hello'
+    //         });
+    //     profileService.getProfile().then((responce) => {
+    //         expect(responce.userAge).toBe(Number);
+    //     });
+    // });
 
 });
