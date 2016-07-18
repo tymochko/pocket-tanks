999f1885ee9c3afe30ab8f63f096af1865839a02
kachanovskyi <vova.kachanovsky@gmail.com>
2016-07-18 19:45:12 +0300

    LVNODQIR-78 added login tests

--- test/client/modules/login/index.spec.js
+++ test/client/modules/login/index.spec.js
@@ -1,35 +1,38 @@
 describe('LoginCtrl', function() {
 
-    var $controller, scope, uibModalInstance, items, sendLog;
+    var $controller, scope, uibModalInstance, items, sendLog, loginResult, httpBackend;
     var controller;
 
     beforeEach(angular.mock.module("tanks.login"));
 
-    describe('Testing sendLog service', function() {
-        var httpBackend;
+    // describe('Testing sendLog service', function() {
+    //     var httpBackend;
 
-        beforeEach(inject(function(_sendLog_, _$httpBackend_) {
-            sendLog = _sendLog_;
-            httpBackend = _$httpBackend_;
-        }));
+    //     beforeEach(inject(function(_sendLog_, _$httpBackend_) {
+    //         sendLog = _sendLog_;
+    //         httpBackend = _$httpBackend_;
+    //     }));
 
-        fit('sendLog.log should be defined', function() {
-            httpBackend.whenRoute('POST', 'api/users/login/');
-            expect(sendLog.log).toBeDefined();
-        });
+    //     fit('sendLog.log should be defined', function() {
+    //         httpBackend.whenRoute('POST', 'api/users/login/');
+    //         expect(sendLog.log).toBeDefined();
+    //     });
 
-    });
+    // });
 
 
-    beforeEach(inject(function($rootScope, _$controller_) {
+    beforeEach(inject(function($rootScope, _$controller_, _loginResult_, _$httpBackend_, _sendLog_) {
         scope = $rootScope.$new();
         $controller = _$controller_;
+        loginResult = _loginResult_;
+        httpBackend = _$httpBackend_;
+        sendLog = _sendLog_;
     }));
 
     beforeEach(inject(function() {
-            sendLog = {};
+            //sendLog = {};
 
-            controller = $controller('LoginCtrl', { $scope: scope, sendLog: sendLog, $uibModalInstance: uibModalInstance, items: items});
+            controller = $controller('LoginCtrl', { $scope: scope, sendLog: sendLog, $uibModalInstance: uibModalInstance, items: items, loginResult: loginResult});
             uibModalInstance = {};
             uibModalInstance.close = function () {
 
@@ -37,41 +40,64 @@ describe('LoginCtrl', function() {
             
             items = [];
     }));
+    
 
     describe('Testing controller variables', function() {
 
         fit('minLengthName', function() {
             expect(scope.minLengthName).toEqual(5);
+            expect(scope.minLengthName).not.toEqual(6);
         });
 
         fit('maxLengthName', function() {
-            expect(scope.maxLengthName).toBeDefined();
+            expect(scope.maxLengthName).toEqual(15);
+            expect(scope.maxLengthName).not.toEqual(12);
         });
 
         fit('minLengthPass', function() {
             expect(scope.minLengthPass).toEqual(6);
+            expect(scope.minLengthPass).not.toEqual(5);
         });
 
         fit('maxLengthPass', function() {
-            expect(scope.maxLengthPass).toBeDefined();
+            expect(scope.maxLengthPass).toEqual(12);
+            expect(scope.maxLengthPass).not.toEqual(15);
         });
 
     });
 
+
     describe('Testing controller functions', function() {
 
         fit("$scope.login check", function () {
-            var succeeded;
+            // var succeeded;
             var userInfo = {
                 userName: 'Jack',
                 userPassword: 'Black'
             };
 
-            sendLog.log = function(userInfo, scope, uibModalInstance, items) {
-                succeeded = true;
+            // sendLog.log = function(userInfo, scope, uibModalInstance, items, loginResult) {
+                
+            // };
+
+            // httpBackend.whenRoute('POST', 'api/users/login/');
+              
+            sendLog.log.success = function() {
+                console.log('SUCCESS');
             };
+            sendLog.log = jasmine.createSpy('sendLog.log.success() spy');
+
+            sendLog.log.error = function() {
+                console.log('ERROR');
+            };
+            sendLog.log.error = jasmine.createSpy('sendLog.log.error() spy');
+            
             
             scope.login(userInfo);
+            // expect(succeeded).toBe(true);
+            expect(sendLog.log).toHaveBeenCalled();
+            // expect(sendlog.log.success).toHaveBeenCalled();
+            // expect(sendlog.log.error).toHaveBeenCalled();
         });
 
     });
