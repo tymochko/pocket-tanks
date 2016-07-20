29928684265d4dd1914446186c9e4abe401f4c09
Yuri Tymochko <rain.yura@gmail.com>
2016-07-20 16:07:00 +0300

    LVNODQIR-92 start create service

---
+++ src/client/modules/serviceSendData/index.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+import ngRoute from 'angular-route';
+module.exports = angular.module('tanks.service', [
+    ngRoute
+])
+    .service('dataService', ['$http', ($http) => {
+        
+    }]);
