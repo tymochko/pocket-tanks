7a6e2d6491bd2f7c8a921863431a61da426f24ce
Yuri Tymochko <rain.yura@gmail.com>
2016-07-22 16:43:47 +0300

    LVNODQIR-92 add $http get request for testing routes

--- src/client/modules/game/GameCtrl.js
+++ src/client/modules/game/GameCtrl.js
@@ -2,6 +2,7 @@ import gameService from './DataTravel';
 import { initGame } from '../../models/tankShot';
 
 export function transportData(gameService) {
-    gameService.getGameData(initGame());
-    gameService.putGameData(126);
+    // gameService.getGameData(initGame());
+    // gameService.putGameData(126);
+    gameService(initGame());
 }
--- src/client/modules/game/index.js
+++ src/client/modules/game/index.js
@@ -14,33 +14,34 @@ module.exports = angular.module('tanks.game', [
     .factory('gameService', ['$http', ($http) => {
         let gameData = {};
 
-        return {
-            getGameData: (msg) => {
-                gameData = msg;
-                console.log(gameData, 'gameData');
-            },
-            
-            putGameData: (paramX) => {
-                return initGame(paramX);
-            }
+        // return {
+        //     getGameData: (msg) => {
+        //         gameData = msg;
+        //         console.log(gameData, 'gameData');
+        //     },
+        //
+        //     putGameData: (paramX) => {
+        //         return initGame(paramX);
+        //     }
 
-            // gameData = msg;
-            // console.log(gameData, 'gameData');
-            //
-            // $http({
-            //     method: 'GET',
-            //     url: 'api/users/profile'
-            // }).then(function successCallback(response) {
-            //     console.log(response.data, 'response.data');
-            //     console.log('success');
-            //     // this callback will be called asynchronously
-            //     // when the response is available
-            // }, function errorCallback(response) {
-            //     console.log(response.data, 'response.data');
-            //     console.log('error');
-            //     // called asynchronously if an error occurs
-            //     // or server returns response with an error status.
-            // });
+        return function (msg) {
+            gameData = msg;
+            console.log(gameData, 'gameData');
+            
+            $http({
+                method: 'GET',
+                url: 'api/users/profile'
+            }).then(function successCallback(response) {
+                console.log(response.data, 'response.data');
+                console.log('success');
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                console.log(response.data, 'response.data');
+                console.log('error');
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
         };
     }])
 ;
