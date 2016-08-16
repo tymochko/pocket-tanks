5fdbc3710f6b2c0742b4d861830ab635f094841c
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-08-16 23:03:36 +0300

    fix session after register

--- src/client/modules/signup/sendRegService.js
+++ src/client/modules/signup/sendRegService.js
@@ -2,18 +2,18 @@ import { toastr } from 'angular-toastr';
 
 export class sendReg {
     constructor($http, $location, toastr) {
-        this.add = function (userInfo) {
+        this.add = function(userInfo) {
             return $http.post('/api/users/add', userInfo).then(() => {
                 toastr.success('You registered successfully \n and can now log in.', 'Congratulations!', {
                     closeButton: true,
                     closeHtml: '<button>&times;</button>'
-                })
+                });
                 $location.path('/');
             }, () => {
                 toastr.warning('Your username or e-mail address \n is taken. Please choose another one.', 'Oops!', {
                     closeButton: true,
                     closeHtml: '<button>&times;</button>'
-                })
+                });
             });
         };
     }
